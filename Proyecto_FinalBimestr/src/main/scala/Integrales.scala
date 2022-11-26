69a9e84f8f4886aec9400b650962949ee5579b4e
Ronin Carrion <rdcarrion3@utpl.edu.ec>
2022-11-26 17:14:51 -0500

    Simpson 1/3

--- Proyecto_FinalBimestr/src/main/scala/Integrales.scala
+++ Proyecto_FinalBimestr/src/main/scala/Integrales.scala
@@ -9,10 +9,17 @@ import scala.io.StdIn.*
     "[3] ʃ((x + 2x^2 - x^3 + 5x^4))\n")
   var option = readLine()
   option match {
-    case "1" => println("Ingrese el valor inferior")
-      var valInf = readInt()
+    case "1" =>
+      val x = (a: Double, b:Double) =>(a+b)/2
+      val f_b = (y:Double)=> -v(y*y)+ (8*y)-12
+      val f_a = (z:Double)=> -v(z*z)+ (8*z)-12
+      val i = (valA: Double, valB:Double) => ((valB - valA) *(f_a(valA)+4*(x(valA, valB))+f_b(valB)))/6
+      println("Ingrese el valor inferior")
+      val valInf = readInt()
       println("Ingrese el valor Superior")
-      var valSup = readInt()
+      val valSup = readInt()
+      val respuesta = i(valInf, valSup)
+      println("Método de Simpson 1/3\n\t" + respuesta)
       //val ec1 : (a:Int, b:Int)=>(a to b)
     case "2" => println("February")
     case "3" => println("March")
